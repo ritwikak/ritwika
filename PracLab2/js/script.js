36a52db6642b4390fabe297ce890ac285977a71f
ritwikak <ritwikakanungoe@hotmail.com>
2017-03-07 14:50:31 +0530

    Update script.js

--- PracLab2/js/script.js
+++ PracLab2/js/script.js
@@ -1,4 +1,9 @@
-function sayHello () {
+function sayHello () 
+{
+  document
+.querySelector("#title")
+.textContent;
+  title ="Lecture 53";
 var name =
 document.getElementById("name").value;
 var message = "<h2>Hello " + name + "!</h2>";
@@ -8,7 +13,8 @@ var message = "<h2>Hello " + name + "!</h2>";
 document
 .getElementById("content")
 .innerHTML = message;
-if (name === "student") {
+if (name === "student") 
+{
 var title =
 document
 .querySelector("#title")
