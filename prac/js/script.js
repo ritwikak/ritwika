54be9bc646e6d4838f088779c6a56f867981aa84
ritwikak <ritwikakanungoe@hotmail.com>
2017-03-20 23:41:34 +0530

    Update script.js

--- prac/js/script.js
+++ prac/js/script.js
@@ -18,5 +18,53 @@ document.addEventListener("DOMContentLoaded",
                 .innerHTML = "<h2>" + message + "</h2>";
             });    
           }); 
+    document.querySelector("#sub")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var a=res.firstnum-res.secondnum;
+              var message = 
+                res.firstnum + " - " + res.secondnum + " = "+ a;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });    
+          }); 
+
+    document.querySelector("#mul")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var a=res.firstnum*res.secondnum;
+              var message = 
+                res.firstnum + " x " + res.secondnum + " = "+ a;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });    
+          }); 
+
+    document.querySelector("#div")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var a=res.firstnum/res.secondnum;
+              var message = 
+                res.firstnum + " / " + res.secondnum + " = "+ a;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });    
+          }); 
+
 }
                          );
