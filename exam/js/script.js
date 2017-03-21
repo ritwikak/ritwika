b52439c22be93c47cf43d82a2a786c31ab41c9eb
ritwikak <ritwikakanungoe@hotmail.com>
2017-03-21 15:00:18 +0530

    Update script.js

--- exam/js/script.js
+++ exam/js/script.js
@@ -1,20 +1,25 @@
 
 
-function getMarks () {
-  var student =
-   document.getElementById("name").value;
-  document.write(student);
- 
-if(name=="Aakriti")
-{
+//function getMarks () {
+  //var student =
+  // document.getElementById("name").value;
+  //document.write(student);
+  
+  var student ;
+
 // feom example 57 - 
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
+  function getMarks () {  
+     var student =   document.getElementById("name").value;
+  }
     
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
+        
+      if(name=="Aakriti"){
         
         // Call server to get the name
         $ajaxUtils
