9bbbec0a7ec247c30580ee7000226082f1067a69
ritwikak <ritwikakanungoe@hotmail.com>
2017-03-21 14:33:22 +0530

    Create script.js

---
+++ exam/js/script.js
@@ -0,0 +1,53 @@
+
+
+function getMarks () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Marks of " + name + "!</h2>";
+
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+
+
+// feom example 57 - 
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesChineseFood) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
+
+
+
+
+
